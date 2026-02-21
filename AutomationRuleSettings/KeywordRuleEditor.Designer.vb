e721d4addeec64479188f38e0a9ef49340114b06
YodeSkye <YodeSkye@outlook.com>
2026-02-21 07:32:43 -0500

    Automation

---
+++ AutomationRuleSettings/KeywordRuleEditor.Designer.vb
@@ -0,0 +1,36 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class KeywordRuleEditor
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' KeywordRuleEditor
+        ' 
+        AutoScaleMode = AutoScaleMode.None
+        Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Name = "KeywordRuleEditor"
+        Size = New Size(415, 253)
+        ResumeLayout(False)
+    End Sub
+
+End Class
---
+++ AutomationRuleSettings/KeywordRuleEditor.vb
@@ -0,0 +1,3 @@
+﻿Public Class KeywordRuleEditor
+
+End Class
