bb1c28b80929cc474c52e5a077b121b9f8254ec5
YodeSkye <YodeSkye@outlook.com>
2026-02-27 09:49:46 -0500

    Rules UI

--- AutomationRuleSettings/SourceAppRuleEditor.Designer.vb
+++ AutomationRuleSettings/SourceAppRuleEditor.Designer.vb
@@ -22,74 +22,133 @@ Partial Class SourceAppRuleEditor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Label1 = New Skye.UI.Label()
-        TxtBoxAppName = New TextBox()
+        components = New ComponentModel.Container()
         BtnSave = New Button()
         CoBoxAction = New Skye.UI.ComboBox()
         Label3 = New Skye.UI.Label()
+        TxtBoxTargetProcess = New TextBox()
+        CoBoxTargetProcess = New Skye.UI.ComboBox()
+        Label1 = New Skye.UI.Label()
+        Tip = New Skye.UI.ToolTipEX(components)
+        TipError = New Skye.UI.ToolTipEX(components)
         SuspendLayout()
-        ' 
-        ' Label1
-        ' 
-        Label1.Location = New Point(112, 53)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(123, 23)
-        Label1.TabIndex = 0
-        Label1.Text = "App Name"
-        ' 
-        ' TxtBoxAppName
-        ' 
-        TxtBoxAppName.Location = New Point(112, 75)
-        TxtBoxAppName.Name = "TxtBoxAppName"
-        TxtBoxAppName.Size = New Size(190, 29)
-        TxtBoxAppName.TabIndex = 1
         ' 
         ' BtnSave
         ' 
+        TipError.SetImage(BtnSave, Nothing)
         BtnSave.Image = My.Resources.Resources.ImageOK16
+        Tip.SetImage(BtnSave, Nothing)
         BtnSave.ImageAlign = ContentAlignment.MiddleLeft
         BtnSave.Location = New Point(3, 214)
         BtnSave.Name = "BtnSave"
         BtnSave.Size = New Size(409, 36)
-        BtnSave.TabIndex = 4
+        BtnSave.TabIndex = 100
+        Tip.SetText(BtnSave, Nothing)
+        TipError.SetText(BtnSave, Nothing)
         BtnSave.Text = "Save Rule"
         BtnSave.UseVisualStyleBackColor = True
         ' 
         ' CoBoxAction
         ' 
+        CoBoxAction.DropDownStyle = ComboBoxStyle.DropDownList
         CoBoxAction.FormattingEnabled = True
-        CoBoxAction.Location = New Point(112, 127)
+        Tip.SetImage(CoBoxAction, Nothing)
+        TipError.SetImage(CoBoxAction, Nothing)
+        CoBoxAction.Location = New Point(109, 126)
         CoBoxAction.Name = "CoBoxAction"
-        CoBoxAction.Size = New Size(190, 30)
-        CoBoxAction.TabIndex = 5
+        CoBoxAction.Size = New Size(197, 30)
+        CoBoxAction.TabIndex = 30
+        TipError.SetText(CoBoxAction, Nothing)
+        Tip.SetText(CoBoxAction, Nothing)
         ' 
         ' Label3
         ' 
-        Label3.Location = New Point(112, 107)
+        TipError.SetImage(Label3, Nothing)
+        Tip.SetImage(Label3, Nothing)
+        Label3.Location = New Point(109, 106)
         Label3.Name = "Label3"
         Label3.Size = New Size(100, 23)
         Label3.TabIndex = 6
+        Tip.SetText(Label3, Nothing)
         Label3.Text = "Action"
+        TipError.SetText(Label3, Nothing)
+        ' 
+        ' TxtBoxTargetProcess
+        ' 
+        TxtBoxTargetProcess.BorderStyle = BorderStyle.None
+        TipError.SetImage(TxtBoxTargetProcess, Nothing)
+        Tip.SetImage(TxtBoxTargetProcess, Nothing)
+        TxtBoxTargetProcess.Location = New Point(113, 82)
+        TxtBoxTargetProcess.Name = "TxtBoxTargetProcess"
+        TxtBoxTargetProcess.Size = New Size(179, 22)
+        TxtBoxTargetProcess.TabIndex = 14
+        Tip.SetText(TxtBoxTargetProcess, Nothing)
+        TipError.SetText(TxtBoxTargetProcess, Nothing)
+        ' 
+        ' CoBoxTargetProcess
+        ' 
+        CoBoxTargetProcess.DropDownStyle = ComboBoxStyle.DropDownList
+        CoBoxTargetProcess.FormattingEnabled = True
+        Tip.SetImage(CoBoxTargetProcess, Nothing)
+        TipError.SetImage(CoBoxTargetProcess, Nothing)
+        CoBoxTargetProcess.ItemHeight = 24
+        CoBoxTargetProcess.Location = New Point(109, 78)
+        CoBoxTargetProcess.Name = "CoBoxTargetProcess"
+        CoBoxTargetProcess.Size = New Size(197, 30)
+        CoBoxTargetProcess.TabIndex = 15
+        CoBoxTargetProcess.TabStop = False
+        TipError.SetText(CoBoxTargetProcess, Nothing)
+        Tip.SetText(CoBoxTargetProcess, Nothing)
+        ' 
+        ' Label1
+        ' 
+        TipError.SetImage(Label1, Nothing)
+        Tip.SetImage(Label1, Nothing)
+        Label1.Location = New Point(109, 56)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(123, 23)
+        Label1.TabIndex = 13
+        Tip.SetText(Label1, Nothing)
+        Label1.Text = "Target Process"
+        TipError.SetText(Label1, Nothing)
+        ' 
+        ' Tip
+        ' 
+        Tip.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Tip.ShadowThickness = 0
+        ' 
+        ' TipError
+        ' 
+        TipError.Font = New Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TipError.HideDelay = 4000
+        TipError.ShadowThickness = 0
         ' 
         ' SourceAppRuleEditor
         ' 
         AutoScaleMode = AutoScaleMode.None
+        Controls.Add(TxtBoxTargetProcess)
+        Controls.Add(CoBoxTargetProcess)
+        Controls.Add(Label1)
         Controls.Add(CoBoxAction)
         Controls.Add(Label3)
         Controls.Add(BtnSave)
-        Controls.Add(TxtBoxAppName)
-        Controls.Add(Label1)
         Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Tip.SetImage(Me, Nothing)
+        TipError.SetImage(Me, Nothing)
         Name = "SourceAppRuleEditor"
         Size = New Size(415, 253)
+        TipError.SetText(Me, Nothing)
+        Tip.SetText(Me, Nothing)
         ResumeLayout(False)
         PerformLayout()
     End Sub
-
-    Friend WithEvents Label1 As Skye.UI.Label
-    Friend WithEvents TxtBoxAppName As TextBox
     Friend WithEvents BtnSave As Button
     Friend WithEvents CoBoxAction As Skye.UI.ComboBox
     Friend WithEvents Label3 As Skye.UI.Label
+    Friend WithEvents TxtBoxTargetProcess As TextBox
+    Friend WithEvents CoBoxTargetProcess As Skye.UI.ComboBox
+    Friend WithEvents Label1 As Skye.UI.Label
+    Friend WithEvents Tip As Skye.UI.ToolTipEX
+    Friend WithEvents TipError As Skye.UI.ToolTipEX
 
 End Class
--- AutomationRuleSettings/SourceAppRuleEditor.vb
+++ AutomationRuleSettings/SourceAppRuleEditor.vb
@@ -1,5 +1,5 @@
 ﻿
-Imports System.Text
+Imports SkyeClip.ClipRepository
 
 Public Class SourceAppRuleEditor
 
@@ -11,16 +11,44 @@ Public Class SourceAppRuleEditor
     Public Sub New()
         InitializeComponent()
 
-        ' Populate actions from enum
+        CoBoxTargetProcess.Items.Clear()
+        For Each a In App.Tray.repo.GetKnownSourceApps()
+            CoBoxTargetProcess.Items.Add(a)
+        Next
+
         Dim actions = [Enum].GetValues(Of ContentAction)().Cast(Of Object).ToArray()
         CoBoxAction.Items.AddRange(actions)
 
     End Sub
+    Private Sub SourceAppRuleEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Skye.UI.ThemeManager.ApplyToTooltip(Tip)
+        Skye.UI.ThemeManager.ApplyToTooltip(TipError)
+    End Sub
 
     ' Control Events
+    Private Sub CoBoxTargetProcess_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CoBoxTargetProcess.SelectedIndexChanged
+        If TypeOf CoBoxTargetProcess.SelectedItem Is SourceAppInfo Then
+            Dim item = CType(CoBoxTargetProcess.SelectedItem, SourceAppInfo)
+            TxtBoxTargetProcess.Text = item.ProcessName
+        End If
+    End Sub
     Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+        Tip.HideTooltip()
+        TipError.HideTooltip()
+        If TxtBoxTargetProcess.Text.Trim() = String.Empty Then
+            Dim pt = CoBoxTargetProcess.PointToScreen(New Point(0, CoBoxTargetProcess.Height))
+            TipError.ShowTooltipAt(pt, "Please enter or select a process name.", My.Resources.ImageRules16)
+            Exit Sub
+        End If
+        If CoBoxAction.SelectedItem Is Nothing Then
+            Dim pt = CoBoxAction.PointToScreen(New Point(0, CoBoxAction.Height))
+            TipError.ShowTooltipAt(pt, "Please select an action.", My.Resources.ImageRules16)
+            Exit Sub
+        End If
+
         SaveRule()
         RaiseEvent RuleSaved(Me, _rule)
+
     End Sub
 
     ' Methods
@@ -28,7 +56,7 @@ Public Class SourceAppRuleEditor
         _rule = rule
 
         ' App name
-        TxtBoxAppName.Text = _rule.AppName
+        TxtBoxTargetProcess.Text = _rule.AppName
 
         ' Action
         CoBoxAction.SelectedItem = _rule.Action
@@ -37,7 +65,7 @@ Public Class SourceAppRuleEditor
     Friend Sub SaveRule()
 
         ' App name
-        _rule.AppName = TxtBoxAppName.Text.Trim()
+        _rule.AppName = TxtBoxTargetProcess.Text.Trim()
 
         ' Action
         _rule.Action = CType(CoBoxAction.SelectedItem, App.ContentAction)
