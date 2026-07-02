5eb3cf699a064b2f1306233dd25a362dff069729
YodeSkye <YodeSkye@outlook.com>
2026-07-02 08:28:54 -0400

    Increase menu font size; update SkyeLibrary to 1.5.0
    Increased MenuFont size from 10 to 12 for context menus in App.vb. Updated SkyeLibrary NuGet package reference from 1.4.69 to 1.5.0 in SkyeClip.vbproj.

--- App.vb
+++ App.vb
@@ -43,7 +43,7 @@ Friend Module App
     Friend ReadOnly CBUnknownFormatString As String = "< Unknown Format >"
     Friend ReadOnly CBRTFSuffix As String = " <RTF>"
     Friend ReadOnly CBHTMLSuffix As String = " <HTML>"
-    Friend ReadOnly MenuFont As New Font("Segoe UI", 10, FontStyle.Regular) ' MenuFont is the font used for context menus.
+    Friend ReadOnly MenuFont As New Font("Segoe UI", 12, FontStyle.Regular) ' MenuFont is the font used for context menus.
     Friend ReadOnly AttributionSkye As String = "https://github.com/YodeSkye/SkyeClip" ' AttributionSkye is the URL for the SkyeClip project.
     Friend ReadOnly AttributionMicrosoft As String = "https://www.microsoft.com" 'AttributionMicrosoft is the URL for Microsoft, which provides various APIs and libraries used in the application.
     Friend ReadOnly AttributionSQLite As String = "https://www.sqlite.org/index.html" 'AttributionSQLite is the URL for SQLite, which provides database functionality in the application.
